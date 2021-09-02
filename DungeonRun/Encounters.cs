e717fe772af5bc8fee45c744ae88629e4c0665aa
allenflo <68171417+allenflo@users.noreply.github.com>
2021-09-02 15:41:03 -0700

    Added Encounters Class, Players Class, and Program Class

---
+++ DungeonRun/Encounters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonRun
+{
+    class Encounters
+    {
+        //Encounter Generic
+
+
+        //Encounters
+        public static void FirstEncounter()
+        {
+            Console.WriteLine("You throw open the door and grab a rusty metal sword, while chargine toward your captor");
+            Console.WriteLine("He turns...");
+            Console.ReadKey();  //wait for them to press a button
+
+
+        }
+
+        //Encounter Tools
+        public static void Combat()
+        { 
+        
+        }
+    }
+
+}
---
+++ DungeonRun/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonRun
+{
+    class Player
+    {
+        public string name;
+        public int coins = 0;
+        public int health = 10;
+        public int damage = 1;
+        public int armorValue = 0;
+        public int potions = 5;
+    }
+}

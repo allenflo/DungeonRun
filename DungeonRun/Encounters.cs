bc50a3663603a0123327d1f2663ecefddbdeb8b0
allenflo <68171417+allenflo@users.noreply.github.com>
2021-09-02 18:21:10 -0700

    Update Encounters.cs

--- DungeonRun/Encounters.cs
+++ DungeonRun/Encounters.cs
@@ -20,9 +20,47 @@ namespace DungeonRun
         }
 
         //Encounter Tools
-        public static void Combat()
-        { 
-        
+        public static void Combat(bool random, string name, int power, int health)
+        {
+            string n = "";
+            int p = 0;
+            int h = 0;
+            if (random)
+            {
+            }
+            else
+            {
+                n = name;
+                p = power;
+                h = health;
+            }
+            while (h > 0)
+            {
+                Console.WriteLine("=======================");
+                Console.WriteLine("| (A)ttack (D)efend   |");
+                Console.WriteLine("| (R)un    (H)eal     |");
+                Console.WriteLine("=======================");
+                Console.WriteLine(" Potions: "+Program.currentPlayer.potions+"  Health: "+Program.currentPlayer.health);
+                string input = Console.ReadLine();
+                if (input.ToLower() == "a" || input.ToLower() == "attack")
+                {
+                    // Attack
+                }
+                else if (input.ToLower() == "d" || input.ToLower() == "defend")
+                {
+                    // Defend              
+                }
+
+                else if (input.ToLower() == "r" || input.ToLower() == "run")
+                {
+                    //Run
+                }
+
+                else if (input.ToLower() == "h" || input.ToLower() == "heal")
+                { 
+                }
+            }
+
         }
     }
 
